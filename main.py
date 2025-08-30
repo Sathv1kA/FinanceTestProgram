89bb3550a1925fc31ae75f39bf0dc997526303f2
Sathv1kA <sathvik.allipuram@gmail.com>
2025-08-30 11:28:30 -0400

    Created a Initial program
     The program uses the yfinance api to get info wanted about stocks and prints it to terminal.

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+plt.style.use('seaborn-v0_8')
+import yfinance as yf
+with open("stock.txt","r") as file:
+    stocks=file.read().splitlines()
+    file.close()
+    pass
+with open("commands.txt","r") as file:
+    commands=file.read().splitlines()
+    file.close()
+    pass
+with open("history.txt","a") as hist:
+    hist.write(datetime.now().strftime("%Y-%m-%d Hours:%H: Mins:%M\n"))
+for curstock in stocks:
+    temp = yf.Ticker(curstock)
+    print(f"---{curstock}---")
+    for actions in commands:
+        print(f"{actions}: {temp.info[actions]}")
+    print(f"Price of {curstock} : {temp.info["currentPrice"]}")
+
+    
